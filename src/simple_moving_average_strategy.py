9ee967962128fa445c88889e3dced8faa1bae718
Pranav Mani <pranav656@gmail.com>
2024-01-01 22:30:41 +0100

    basic setup for unit-tests

--- src/simple_moving_average_strategy.py
+++ src/simple_moving_average_strategy.py
@@ -1,6 +1,5 @@
 
 from abc import ABC, abstractmethod
-from collections import deque
 import pandas as pd
    
 class BaseStrategy(ABC):
@@ -38,23 +37,23 @@ class SimpleMovingAverageStrategy(BaseStrategy):
         self.execute_trades()
 
 # create a list and push dummy data into it
-start_price_data = {
-    'Date': pd.date_range(start='2023-01-01', periods=100),
-    'Close': [189 + i for i in range(100)]
-}
+#start_price_data = {
+#    'Date': pd.date_range(start='2023-01-01', periods=100),
+#    'Close': [189 + i for i in range(100)]
+#}
 
 # test strategy for dummy data points based on 20 point
 # moving average, sells on a dowanard trend and buys on
 # an upward trend
-df = pd.DataFrame(start_price_data)
-strategy = SimpleMovingAverageStrategy(df, 20)
-strategy.calculate_signals()
-
-for i in range(100):
-    dummy_current_data = {
-        'Date': pd.date_range(start='2023-01-01', periods=1),
-        'Close': [95+i]
-    }
-    strategy.update_data(pd.DataFrame(dummy_current_data))
-    strategy.calculate_signals()
-    strategy.execute_trades()
+#df = pd.DataFrame(start_price_data)
+#strategy = SimpleMovingAverageStrategy(df, 20)
+#strategy.calculate_signals()
+#
+#for i in range(100):
+#    dummy_current_data = {
+#        'Date': pd.date_range(start='2023-01-01', periods=1),
+#        'Close': [95+i]
+#    }
+#    strategy.update_data(pd.DataFrame(dummy_current_data))
+#    strategy.calculate_signals()
+#    strategy.execute_trades()
---
+++ test/test_simple_moving_average_strategy.py
@@ -0,0 +1,26 @@
+from src.simple_moving_average_strategy import SimpleMovingAverageStrategy
+import pandas as pd
+
+def test_simple_moving_average_strategy():
+    # create a list and push dummy data into it
+    start_price_data = {
+        'Date': pd.date_range(start='2023-01-01', periods=100),
+        'Close': [189 + i for i in range(100)]
+    }
+
+    # test strategy for dummy data points based on 20 point
+    # moving average, sells on a dowanard trend and buys on
+    # an upward trend
+    df = pd.DataFrame(start_price_data)
+    strategy = SimpleMovingAverageStrategy(df, 20)
+    strategy.calculate_signals()
+
+    for i in range(100):
+        dummy_current_data = {
+            'Date': pd.date_range(start='2023-01-01', periods=1),
+            'Close': [95+i]
+        }
+        strategy.update_data(pd.DataFrame(dummy_current_data))
+        strategy.calculate_signals()
+        strategy.execute_trades()
+        assert strategy.signal is not None
