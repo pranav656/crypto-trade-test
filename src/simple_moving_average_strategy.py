96ca5c0edf3ac153e0a6dee2c692d5f2793ded92
Pranav Mani <pranav656@gmail.com>
2024-01-01 22:35:08 +0100

    remove tests from module

--- src/simple_moving_average_strategy.py
+++ src/simple_moving_average_strategy.py
@@ -35,25 +35,3 @@ class SimpleMovingAverageStrategy(BaseStrategy):
     def run_strategy(self):
         self.calculate_signals()
         self.execute_trades()
-
-# create a list and push dummy data into it
-#start_price_data = {
-#    'Date': pd.date_range(start='2023-01-01', periods=100),
-#    'Close': [189 + i for i in range(100)]
-#}
-
-# test strategy for dummy data points based on 20 point
-# moving average, sells on a dowanard trend and buys on
-# an upward trend
-#df = pd.DataFrame(start_price_data)
-#strategy = SimpleMovingAverageStrategy(df, 20)
-#strategy.calculate_signals()
-#
-#for i in range(100):
-#    dummy_current_data = {
-#        'Date': pd.date_range(start='2023-01-01', periods=1),
-#        'Close': [95+i]
-#    }
-#    strategy.update_data(pd.DataFrame(dummy_current_data))
-#    strategy.calculate_signals()
-#    strategy.execute_trades()
